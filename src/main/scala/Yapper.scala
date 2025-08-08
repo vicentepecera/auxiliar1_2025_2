a8d7bcd73aef596314103391660784ba26c7248b
Vicente Cepeda <vicente.cepeda@ug.uchile.cl>
2025-08-08 17:32:24 -0400

    cambiomomerodecorazon

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,7 +3,7 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("Hello my fellow momeros")
+    println("Hello my fellow momerosdecorazon")
   }
 
   def say(something:String):Unit={
